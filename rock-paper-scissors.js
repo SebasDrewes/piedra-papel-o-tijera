a3630e46497a41c044f7a55059be11169b2d5c0d
Sebastian Drewes <sebas.drewes@gmail.com>
2021-04-18 16:36:06 -0300

    updated

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,6 +13,7 @@ function playRound(playerSelection, computerSelection) {
     const resultRound = document.querySelector("#resultRound");
     const jugadorPlay = document.querySelector("#jugadorPlay");
     const compuPlay = document.querySelector("#compuPlay");
+    const body = document.querySelector("#body");
 
     if (playerSelection === computerSelection) {
         resultRound.textContent = "Empataste la ronda!";
@@ -20,48 +21,54 @@ function playRound(playerSelection, computerSelection) {
         compuPlay.textContent = "La compu eligio: " + computerSelection;
     } else if (playerSelection === "piedra" && computerSelection === "tijera") {
         ++winsPlayer
-        pwins.textContent = winsPlayer
+        pwins.textContent = "Rondas ganadas: " + winsPlayer;
         resultRound.textContent = "Ganaste la ronda!";
         jugadorPlay.textContent = "Elegiste: " + playerSelection;
         compuPlay.textContent = "La compu eligio: " + computerSelection;
     } else if (playerSelection === "tijera" && computerSelection === "papel") {
         ++winsPlayer
-        pwins.textContent = winsPlayer
+        pwins.textContent = "Rondas ganadas: " + winsPlayer;
         resultRound.textContent = "Ganaste la ronda!";
         jugadorPlay.textContent = "Elegiste: " + playerSelection;
         compuPlay.textContent = "La compu eligio: " + computerSelection;
     } else if (playerSelection === "papel" && computerSelection === "piedra") {
         ++winsPlayer
-        pwins.textContent = winsPlayer
+        pwins.textContent = "Rondas ganadas: " + winsPlayer;
         resultRound.textContent = "Ganaste la ronda!";
         jugadorPlay.textContent = "Elegiste: " + playerSelection;
         compuPlay.textContent = "La compu eligio: " + computerSelection;
     } else if (playerSelection === "tijera" && computerSelection === "piedra") {
         ++winsComputer
-        cwins.textContent = winsComputer
+        cwins.textContent = "Rondas ganadas por la compu: " + winsComputer;
         resultRound.textContent = "Perdiste la ronda!";
         jugadorPlay.textContent = "Elegiste: " + playerSelection;
         compuPlay.textContent = "La compu eligio: " + computerSelection;
     } else if (playerSelection === "papel" && computerSelection === "tijera") {
         ++winsComputer
-        cwins.textContent = winsComputer
+        cwins.textContent = "Rondas ganadas por la compu: " + winsComputer;
         resultRound.textContent = "Perdiste la ronda!";
         jugadorPlay.textContent = "Elegiste: " + playerSelection;
         compuPlay.textContent = "La compu eligio: " + computerSelection;
     } else if (playerSelection === "piedra" && computerSelection === "papel") {
         ++winsComputer
-        cwins.textContent = winsComputer
+        cwins.textContent = "Rondas ganadas por la compu: " + winsComputer;
         resultRound.textContent = "Perdiste la ronda!";
         jugadorPlay.textContent = "Elegiste: " + playerSelection;
         compuPlay.textContent = "La compu eligio: " + computerSelection;              
     }
     if (winsPlayer === 5 ) {
-        resultRound.textContent = ("Ganaste!!")
+        resultRound.textContent = ("Ganaste!!");
+        botonPapel.style.visibility = 'hidden'; 
+        botonPiedra.style.visibility = 'hidden'; 
+        botonTijera.style.visibility = 'hidden'; 
         document.getElementById("papel").disabled = true;
         document.getElementById("piedra").disabled = true;
         document.getElementById("tijera").disabled = true;
     } else if (winsComputer === 5 ) {
-        resultRound.textContent = ("Perdiste!")
+        resultRound.textContent = ("Perdiste!");
+        botonPapel.style.visibility = 'hidden'; 
+        botonPiedra.style.visibility = 'hidden'; 
+        botonTijera.style.visibility = 'hidden'; 
         document.getElementById("papel").disabled = true;
         document.getElementById("piedra").disabled = true;
         document.getElementById("tijera").disabled = true;
@@ -70,11 +77,14 @@ function playRound(playerSelection, computerSelection) {
 function startNewGame() {
     winsPlayer = 0;
     winsComputer = 0;
-    pwins.textContent = winsPlayer;
-    cwins.textContent = winsComputer;
+    pwins.textContent = "Rondas ganadas: 0";
+    cwins.textContent = "Rondas ganadas por la compu: 0";
     document.getElementById("papel").disabled = false;
     document.getElementById("piedra").disabled = false;
     document.getElementById("tijera").disabled = false;
+    botonPapel.style.visibility = 'visible'; 
+    botonPiedra.style.visibility = 'visible'; 
+    botonTijera.style.visibility = 'visible'; 
     resultRound.textContent = "¿Piedra, papel o tijera?";  
     jugadorPlay.textContent = "";
     compuPlay.textContent = ""; 
